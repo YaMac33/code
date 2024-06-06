24c39ba1f083de250e835fefbc2ffecd98f859ef
Yamac33 <mountflat0104@gmail.com>
2024-06-07 07:48:24 +0900

    edit create.php & new.php & views/new.php

--- part2/src/create.php
+++ part2/src/create.php
@@ -72,7 +72,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     ];
 
     $errors = validate($review);
-    
+
     if(!count($errors)) {
         $link = dbConnect();
         createReview($link, $review);
@@ -80,58 +80,5 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         header("Location: index.php");
     }
 }
-?>
-<!DOCTYPE html>
-<html lang="ja">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>読書ログ</title>
-</head>
-
-<body>
-    <h1>読書ログ</h1>
-    <form action="create.php" method="POST">
-        <?php if(count($errors)) : ?>
-            <ul>
-                <?php foreach ($errors as $error) : ?>
-                    <li><?php echo $error; ?></li>
-                <?php endforeach; ?>
-            </ul>
-        <?php endif; ?>
-        <div>
-            <label for="title">書籍名</label>
-            <input type="text" id="title" name="title">
-        </div>
-        <div>
-            <label for="author">著者名</label>
-            <input type="text" id="author" name="author">
-        </div>
-        <div>
-            <label for="stasus">読書状況</label>
-            <div>
-                <input type="radio"name="status"id="status1"value="未読">
-                <label for="stasus1">未読</label>
-                <input type="radio"name="status"id="status2"value="読んでる">
-                <label for="status2">読んでる</label>
-                <input type="radio"name="status"id="status3"value="読了">
-                <label for="status3">読了</label>
-            </div>
-        </div>
-        <div>
-            <label for="score">評価(5点満点の整数)</label>
-            <input type="text" size="2" id="score" name="score">
-        </div>
-        <div>
-            <label for="summary">感想</label>
-            <input type="text" size="100" id="summary" name="summary">
-        </div>
-        <div>
-            <button type="submit">登録する</button>
-        </div>
-    </form>
-</body>
-
-</html>
-
+include 'views/new.php';
--- part2/src/views/new.php
+++ part2/src/views/new.php
@@ -2,57 +2,51 @@
 <html lang="ja">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>読書ログ登録</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>読書ログ</title>
 </head>
 
 <body>
     <h1>読書ログ</h1>
-    <form action="create.php" method="post">
-        <?php if (count($errors)) : ?>
+    <form action="create.php" method="POST">
+        <?php if(count($errors)) : ?>
             <ul>
                 <?php foreach ($errors as $error) : ?>
                     <li><?php echo $error; ?></li>
                 <?php endforeach; ?>
             </ul>
         <?php endif; ?>
-
         <div>
             <label for="title">書籍名</label>
-            <input type="text" name="title" id="title">
+            <input type="text" id="title" name="title" value="<?php echo $review['title'] ?>">
         </div>
         <div>
             <label for="author">著者名</label>
-            <input type="text" name="author" id="author">
+            <input type="text" id="author" name="author" value="<?php echo $review['author'] ?>">
         </div>
         <div>
-            <label>読書状況</label>
+            <label for="stasus">読書状況</label>
             <div>
-                <div>
-                    <input type="radio" name="status" id="status1" value="未読">
-                    <label for="status1">未読</label>
-                </div>
-                <div>
-                    <input type="radio" name="status" id="status2" value="読んでる">
-                    <label for="status2">読んでる</label>
-                </div>
-                <div>
-                    <input class="form-check-input" type="radio" name="status" id="status3" value="読了">
-                    <label for="status3">読了</label>
-                </div>
+                <input type="radio"name="status"id="status1"value="未読" <?php echo ($review['status'] === '未読') ? 'checked' : ''; ?>>
+                <label for="stasus1">未読</label>
+                <input type="radio"name="status"id="status2"value="読んでる" <?php echo ($review['status'] === '読んでる') ? 'checked' : ''; ?>>
+                <label for="status2">読んでる</label>
+                <input type="radio"name="status"id="status3"value="読了" <?php echo ($review['status'] === '読了') ? 'checked' : ''; ?>>
+                <label for="status3">読了</label>
             </div>
         </div>
         <div>
-            <label for="score">評価（5点満点の整数）</label>
-            <input type="number" name="score" id="score">
+            <label for="score">評価(5点満点の整数)</label>
+            <input type="text" size="2" id="score" name="score" value="<?php echo $review['score'] ?>">
         </div>
         <div>
             <label for="summary">感想</label>
-            <textarea type="text" name="summary" id="summary" rows="10"></textarea>
+            <input type="text" size="100" id="summary" name="summary" value="<?php echo $review['summary'] ?>">
         </div>
-        <button type="submit">登録する</button>
+        <div>
+            <button type="submit">登録する</button>
+        </div>
     </form>
 </body>
 
