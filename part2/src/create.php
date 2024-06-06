750900350d4459af6108bb40ef434b146889e990
Yamac33 <mountflat0104@gmail.com>
2024-06-07 08:04:27 +0900

    edit create.php

--- part2/src/create.php
+++ part2/src/create.php
@@ -18,9 +18,7 @@ INSERT INTO reviews (
     "{$review['score']}",
     "{$review['summary']}"
 )
-EOT;
-
-    $result = mysqli_query($link, $sql);
+EOT;$result = mysqli_query($link, $sql);
     if (!$result) {
         error_log('Error: fail to create review');
         error_log('Debugging Error: ' . mysqli_error($link));
@@ -31,49 +29,57 @@ function validate($review)
 {
     $errors = [];
 
-	if (!mb_strlen($review['title'])){
-		$errors['title'] = '書籍名を入力してください';
-	} elseif (mb_strlen($review['title']) > 255) {
-		$errors['title'] = '書籍名は255文字以内で入力してください';
-	}
-
-    if (!mb_strlen($review['author'])){
-		$errors['author'] = '著者名を入力してください';
-	} elseif (mb_strlen($review['author']) > 100) {
-		$errors['author'] = '著者名は100文字以内で入力してください';
-	}
-
-    if (!mb_strlen($review['status'])){
-		$errors['status'] = '読書状況は「未読」「読んでる」「読了」から選んでください';
-	} elseif (mb_strlen($review['status']) > 10) {
-		$errors['status'] = '読書状況は「未読」「読んでる」「読了」から選んでください';
-	}
-
-    if ($review['score'] < 1 || $review['score'] > 5) {
-        $errors['score'] = '評価は1～5の整数を入力してください';
+    // 書籍名が正しく入力されているかチェック
+    if (!strlen($review['title'])) {
+        $errors['title'] = '書籍名を入力してください';
+    } elseif (strlen($review['title']) > 255) {
+        $errors['title'] = '書籍名は255文字以内で入力してください';
     }
 
-    if (!mb_strlen($review['summary'])){
-		$errors['summary'] = '感想を入力してください';
-	} elseif (mb_strlen($review['summary']) > 1000) {
-		$errors['summary'] = '感想は1000文字以内で入力してください';
-	}
+    // 著者名が正しく入力されているかチェック
+    if (!strlen($review['author'])) {
+        $errors['author'] = '著者名を入力してください';
+    } elseif (strlen($review['author']) > 255) {
+        $errors['author'] = '著者名は255文字以内で入力してください';
+    }
 
-	return $errors;
+    // 読書状況が正しく入力されているかチェック
+    if (!in_array($review['status'], ['未読', '読んでる', '読了'])) {
+        $errors['status'] = '読書状況は「未読」「読んでる」「読了」のいずれかを入力してください';
+    }
+
+    // 評価が正しく入力されているかチェック
+    if ($review['score'] < 1 || $review['score'] > 5) {
+        $errors['score'] = '評価は1〜5の整数を入力してください';
+    }
+
+    // 感想が正しく入力されているかチェック
+    if (!strlen($review['summary'])) {
+        $errors['summary'] = '感想を入力してください';
+    } elseif (strlen($review['summary']) > 10000) {
+        $errors['summary'] = '感想は10,000文字以内で入力してください';
+    }
+
+    return $errors;
 }
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $status = '';
+    if (array_key_exists('status', $_POST)) {
+        $status = $_POST['status'];
+    }
+
     $review = [
         'title' => $_POST['title'],
         'author' => $_POST['author'],
-        'status' => $_POST['status'],
+        'status' => $status,
         'score' => $_POST['score'],
         'summary' => $_POST['summary']
     ];
 
     $errors = validate($review);
 
-    if(!count($errors)) {
+    if (!count($errors)) {
         $link = dbConnect();
         createReview($link, $review);
         mysqli_close($link);
@@ -81,4 +87,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 }
 
-include 'views/new.php';
+$siteTitle = '読書ログの登録';
+$content = __DIR__ . '/views/new.php';
+include __DIR__ . '/views/layout.php';
