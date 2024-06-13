34e6eb7d8c404528bfc26ace1fb5109fa157c329
Yamac33 <mountflat0104@gmail.com>
2024-06-14 06:25:15 +0900

    modified new.php & views/new.php

--- part2/src/new.php
+++ part2/src/new.php
@@ -1,5 +1,14 @@
 <?php
 
+// ここを追記
+$review = [
+    'title' => '',
+    'author' => '',
+    'status' => '未読',
+    'score' => '',
+    'summary' => ''
+];
+
 $errors = [];
 
 include 'views/new.php';
