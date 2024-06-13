c1605f65abd51ced24fc92274ae48acbd5823dad
Yamac33 <mountflat0104@gmail.com>
2024-06-13 20:12:02 +0900

    separate php and html

--- part2/src/companies/new.php
+++ part2/src/companies/new.php
@@ -1,32 +1,5 @@
-<?php
-?>
-<!DOCTYPE html>
-<html lang="ja">
+<?
 
-<head>
-</head>
+$errors = [];
 
-<body>
-<h1>会社情報の登録</h1>
-    <form action="create.php"
-    method="POST">
-        <div>
-            <label for="name">会社名</label>
-            <input type="text" id="name" name="name">
-        </div>
-        </div>
-        <div>
-            <label for="establishment_date">設立日</label>
-            <input type="date" id="establishment_date" name="establishment_date">
-        </div>
-        <div>
-            <label for="founder">代表者</label>
-            <input type="text" id="founder" name="founder">
-        </div>
-            <button type="submit">登録する</button>
-        </div>
-	</form>
-</body>
-
-</html>
-
+include 'views/new.php';
